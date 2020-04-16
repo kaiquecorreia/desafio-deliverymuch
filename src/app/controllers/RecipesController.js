085bc7d6f38a0b65178bafe5802409bcb9f0364f
Kaique Correia <kaique.rc.tl@gmail.com>
2020-04-16 19:34:23 -0300

    Add redis cache and set enviroment to tests

--- src/app/controllers/RecipesController.js
+++ src/app/controllers/RecipesController.js
@@ -1,35 +1,45 @@
 import RecipesServices from '../services/RecipesServices';
 import GiphyServices from '../services/GiphyServices';
+import RedisServices from '../services/RedisServices';
 import { getIngredientsKeyWords } from '../utils/recipes';
 import ErrorHandler from '../error/ErrorHandler';
 class RecipesController {
   constructor() {
     this.recipesService = new RecipesServices(GiphyServices);
+    this.redisService = new RedisServices();
   }
 
   list = async (request, response) => {
+    let result = null;
     const { i: ingredient } = request.query;
+    const cacheRecipes = await this.redisService.getRedisData(ingredient);
+    if (!cacheRecipes) {
+      const keywords = getIngredientsKeyWords(ingredient);
 
-    const keywords = getIngredientsKeyWords(ingredient);
+      if (ErrorHandler.hasError(keywords)) {
+        return ErrorHandler.responseError(response, keywords);
+      }
 
-    if (ErrorHandler.hasError(keywords)) {
-      return ErrorHandler.responseError(response, keywords);
+      const recipesFound = await this.recipesService.getRecipes(ingredient);
+
+      if (ErrorHandler.hasError(recipesFound)) {
+        return ErrorHandler.responseError(response, recipesFound);
+      }
+      const recipesWithGiphys = await this.recipesService.getRecipesWithGif(
+        recipesFound
+      );
+
+      if (ErrorHandler.hasError(recipesWithGiphys)) {
+        return ErrorHandler.responseError(response, recipesWithGiphys);
+      }
+      result = { keywords, recipes: recipesWithGiphys };
+
+      await this.redisService.setRedisData(ingredient, result);
+    } else {
+      result = JSON.parse(cacheRecipes);
     }
 
-    const recipesFound = await this.recipesService.getRecipes(ingredient);
-
-    if (ErrorHandler.hasError(recipesFound)) {
-      return ErrorHandler.responseError(response, recipesFound);
-    }
-    const recipesWithGiphys = await this.recipesService.getRecipesWithGif(
-      recipesFound
-    );
-
-    if (ErrorHandler.hasError(recipesWithGiphys)) {
-      return ErrorHandler.responseError(response, recipesWithGiphys);
-    }
-
-    return response.status(200).json({ keywords, recipes: recipesWithGiphys });
+    return response.status(200).json(result);
   };
 }
 
